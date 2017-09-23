c60bbfaa6137d2c39e778d4966db7fa76e303a41
Eulerd <dicenull.dev@gmail.com>
2017-09-23 18:45:02 +0900

    マーカーの取得実装を変更

--- EffectsFromGesture/Assets/KinectView/Scripts/Calibration/Calibration.cs
+++ EffectsFromGesture/Assets/KinectView/Scripts/Calibration/Calibration.cs
@@ -20,8 +20,8 @@ class Calibration : MonoBehaviour
         _ColorManager = ColorManager.GetComponent<ColorSourceManagerForOpenCV>();
 
         temp = new Mat(@"Assets\Resources\Emotionic_e_marker.png", ImreadModes.GrayScale);
-        Cv2.ImShow("before temp", temp);
         Cv2.Resize(temp, temp, new Size(100, 100));
+
         Cv2.ImShow("temp", temp);
     }
 
@@ -34,6 +34,7 @@ class Calibration : MonoBehaviour
         }
 
         image = _ColorManager.ColorImage;
+        Cv2.Flip(image, image, FlipMode.Y);
         bin = new Mat(image.Size(), MatType.CV_32FC1);
         Cv2.CvtColor(image, bin, ColorConversionCodes.BGR2GRAY);
         
@@ -41,15 +42,35 @@ class Calibration : MonoBehaviour
 
         Cv2.MatchTemplate(bin, temp, result, TemplateMatchModes.CCoeffNormed);
 
-        float threshold = 0.8f;
-        Point maxPt, minPt;
-        Cv2.Threshold(result, result, threshold, 1.0, ThresholdTypes.Binary);
+        double threshold = 0.8;
+        Cv2.Threshold(result, result, threshold, 1.0, ThresholdTypes.Tozero);
 
-        Cv2.MinMaxLoc(result, out minPt, out maxPt);
-
-        image.Rectangle(maxPt, new Point(maxPt.X + temp.Width, maxPt.Y + temp.Height), Scalar.Red);
+        for (int y = 0; y < result.Height; y++)
+        {
+            for (int x = 0; x < result.Width; x++)
+            {
+                if (result.At<int>(y, x) > threshold)
+                {
+                    Debug.Log(new Point(x, y));
+                    detectedPos.Add(new Point(x, y));
+                }
+            }
+        }
         
+        Point endpt;
+        foreach (Point pos in detectedPos)
+        {
+            endpt = new Point(pos.X + temp.Rows, pos.Y + temp.Cols);
+
+            Cv2.Rectangle(image, pos, endpt, Scalar.Red);
+        }
+
         Cv2.ImShow("result image", image);
+    }
+
+    private void OnApplicationQuit()
+    {
+        Cv2.DestroyAllWindows();
     }
 }
 
