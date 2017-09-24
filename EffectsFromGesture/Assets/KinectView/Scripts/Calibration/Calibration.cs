cf67049483c72ce9e09204a52cf12853d32c1a8b
Eulerd <dicenull.dev@gmail.com>
2017-09-24 13:06:26 +0900

    コメントの追加

--- EffectsFromGesture/Assets/KinectView/Scripts/Calibration/Calibration.cs
+++ EffectsFromGesture/Assets/KinectView/Scripts/Calibration/Calibration.cs
@@ -8,40 +8,65 @@ using OpenCvSharp.Util;
 
 class Calibration : MonoBehaviour
 {
+    /// <summary>
+    /// 画像Managerのインスタンス取得用
+    /// </summary>
     public GameObject ColorManager;
 
+    /// <summary>
+    /// Kinectの画像をMat型に変換したオブジェクト取得用
+    /// </summary>
     private ColorSourceManagerForOpenCV _ColorManager;
 
-    List<Point> detectedPos = new List<Point>();
-    Mat rawTemp, temp, image, result, bin;
+    /// <summary>
+    /// マーカーを検出した場所
+    /// </summary>
+    private List<Point> detectedPos = new List<Point>();
+
+    /// <summary>
+    /// パターンマッチング用
+    /// </summary>
+    private Mat rawTemp, temp, image, result, bin;
+    
+    private int count = 0;
+    private double threshold = 0.5;
 
     private void Start()
     {
+        // インスタンスを取得
         _ColorManager = ColorManager.GetComponent<ColorSourceManagerForOpenCV>();
 
+        // 検出するマーカー読み込み
         rawTemp = new Mat(@"Assets\Resources\Emotionic_e_marker.png", ImreadModes.GrayScale);
     }
 
-    private int count = 0;
-
     private void Update()
     {
+        // managerからKinect画像を取得
         image = _ColorManager.ColorImage;
+
+        // 左右反転していたので元に戻す
         Cv2.Flip(image, image, FlipMode.Y);
-        // Cv2.Resize(image, image, new Size(image.Size().Width / 3, image.Size().Height / 3));
+        
+        // Kinect画像を白黒に変換
         bin = new Mat(image.Size(), MatType.CV_32FC1);
         Cv2.CvtColor(image, bin, ColorConversionCodes.BGR2GRAY);
 
+        // マーカーを拡大縮小
         temp = new Mat();
         Cv2.Resize(rawTemp, temp, new Size(100 + count * 3, 100 + count * 3));
 
+        // 検出結果用Matの準備
         result = new Mat(bin.Rows - temp.Rows + 1, bin.Cols - temp.Cols + 1, MatType.CV_32FC1);
 
+        // パターンマッチング
         Cv2.MatchTemplate(bin, temp, result, TemplateMatchModes.CCoeffNormed);
 
-        double threshold = 0.5;
+        // 閾値以下を捨てる
         Cv2.Threshold(result, result, threshold, 1.0, ThresholdTypes.Tozero);
 
+        // 検出した座標をListに追加
+        // 検出したらその範囲にはもうないので飛ばす
         bool isAdded;
         for (int y = 0; y < result.Height; y++)
         {
@@ -61,6 +86,7 @@ class Calibration : MonoBehaviour
                 y += temp.Height - 1;
         }
 
+        // 検出結果を赤い四角で描写
         Point endpt;
         foreach (Point pos in detectedPos)
         {
@@ -69,10 +95,14 @@ class Calibration : MonoBehaviour
             Cv2.Rectangle(image, pos, endpt, Scalar.Red);
         }
 
+        // マーカーと検出結果を表示
         Cv2.ImShow("temp", temp);
         Cv2.ImShow("result", image);
+
+        // 検出座標初期化
         detectedPos.Clear();
 
+        // マーカーの大きさを変えて再度検出
         count = (count + 1) % 10;
 
         Debug.Log("----" + count + "----");
