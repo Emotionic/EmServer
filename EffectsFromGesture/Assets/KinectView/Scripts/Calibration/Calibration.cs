8ae01d5747239707538b7ce74664f12df964881b
Eulerd <dicenull.dev@gmail.com>
2017-09-25 18:39:13 +0900

    元に戻してラベリングで検出

--- EffectsFromGesture/Assets/KinectView/Scripts/Calibration/Calibration.cs
+++ EffectsFromGesture/Assets/KinectView/Scripts/Calibration/Calibration.cs
@@ -3,6 +3,7 @@ using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.UI;
 using OpenCvSharp;
+using System.Linq;
 
 class Calibration : MonoBehaviour
 {
@@ -16,6 +17,58 @@ class Calibration : MonoBehaviour
 
     private Texture2D texture;
 
+    private Mat ColorExtraction(InputArray src, ColorConversionCodes code,
+        int ch1Lower, int ch1Upper, int ch2Lower, int ch2Upper, int ch3Lower, int ch3Upper)
+    {
+        if (src == null)
+            throw new ArgumentException("src");
+
+        Mat colorMat = new Mat(src.Size(), MatType.CV_8UC3);
+        Cv2.CvtColor(src, colorMat, code);
+
+        Mat lut = new Mat(256, 1, MatType.CV_8UC3);
+
+        int[] lower = { ch1Lower, ch2Lower, ch3Lower };
+        int[] upper = { ch1Upper, ch2Upper, ch3Upper };
+
+        var mat3 = new MatOfByte3(lut);
+
+        var indexer = mat3.GetIndexer();
+
+        for (int i = 0; i < 256; i++)
+        {
+            Vec3b color = indexer[i];
+            byte temp;
+
+            for (int k = 0; k < 3; k++)
+            {
+                if (lower[k] <= upper[k])
+                {
+                    temp = (byte)((lower[k] <= i && i <= upper[k]) ? 255 : 0);
+                }
+                else
+                {
+                    temp = (byte)((i <= upper[k] || lower[k] <= i) ? 255 : 0);
+                }
+
+                color[k] = temp;
+            }
+
+            indexer[i] = color;
+        }
+
+        Cv2.LUT(colorMat, lut, colorMat);
+
+        var channelMat = colorMat.Split();
+
+        var maskMat = new Mat();
+
+        Cv2.BitwiseAnd(channelMat[0], channelMat[1], maskMat);
+        Cv2.BitwiseAnd(maskMat, channelMat[2], maskMat);
+
+        return maskMat;
+    }
+
     public void ColorExtraction(Mat srcImage, Mat dstImage, ColorConversionCodes code,
         int ch1Lower, int ch1Upper, int ch2Lower, int ch2Upper, int ch3Lower, int ch3Upper)
     {
@@ -24,6 +77,7 @@ class Calibration : MonoBehaviour
         else if (dstImage == null)
             throw new ArgumentNullException("dstImage");
 
+        Debug.Log(srcImage.Type());
 
         Mat colorImage;
         Mat[] ch3sImage = new Mat[3];
@@ -36,7 +90,8 @@ class Calibration : MonoBehaviour
 
         Mat lut;
 
-        colorImage = new Mat(srcImage.Size(), srcImage.Depth(), srcImage.Channels());
+        colorImage = new Mat(srcImage.Size(), MatType.CV_8U);
+        // colorImage = new Mat(srcImage.Size(), srcImage.Depth(), srcImage.Channels());
         Cv2.CvtColor(srcImage, colorImage, code);
 
         lut = new Mat(256, 1, MatType.CV_8UC3);
@@ -79,7 +134,11 @@ class Calibration : MonoBehaviour
             lut.Set(i, new Scalar(val[0], val[1], val[2]));
         }
 
-        Cv2.LUT(colorImage, colorImage, lut);
+        Debug.Log(colorImage.Type());
+        Debug.Log(lut.Type());
+
+        Cv2.LUT(colorImage, lut, colorImage);
+        
         lut.Release();
 
         ch3sImage[0] = new Mat(colorImage.Size(), colorImage.Depth(), 1);
@@ -91,9 +150,9 @@ class Calibration : MonoBehaviour
         maskImage = new Mat(colorImage.Size(), colorImage.Depth(), 1);
         Cv2.BitwiseAnd(ch3sImage[0], ch3sImage[1], maskImage);
         Cv2.BitwiseAnd(maskImage, ch3sImage[2], maskImage);
-        
+
         // Cv2.Zero(dstImage);
-        srcImage.CopyTo(dstImage, maskImage);
+        maskImage.CopyTo(dstImage);
 
         colorImage.Release();
         ch3sImage[0].Release();
@@ -129,14 +188,28 @@ class Calibration : MonoBehaviour
         texture.Apply();
 
         // 青色を検出
-        ColorExtraction(image, dstImage, ColorConversionCodes.BGR2HSV, 90, 110, 200, 255, 200, 255);
-        
+        var skinMat = ColorExtraction(image, ColorConversionCodes.BGR2HSV, 110, 130, 0, 255, 0, 255);
+        // ColorExtraction(image, dstImage, ColorConversionCodes.BGR2HSV, 0, 255, 0, 255, 0, 255);
+
+        ConnectedComponents cc = Cv2.ConnectedComponentsEx(skinMat);
+
+        Cv2.PutText(image, x.ToString(), new Point(10, 180), HersheyFonts.HersheyComplexSmall, 1, new Scalar(255, 0, 255));
+        foreach (var blob in cc.Blobs.Skip(1))
+        {
+            if (blob.Area < 100)
+                continue;
+
+            image.Rectangle(blob.Rect, Scalar.Red);
+        }
+
         Cv2.ImShow("image", image);
-        Cv2.ImShow("result", dstImage);
+        Cv2.ImShow("result", skinMat);
 
         texImage.Dispose();
         // image.Dispose();
+        x = (x + 1) % 245;
     }
+    int x = 0;
 
     private void OnApplicationQuit()
     {
